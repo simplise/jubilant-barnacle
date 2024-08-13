04863d0311371000720f28b2bf5241ac50b85527
KoujiSano <sano@simplise.jp>
2024-08-13 17:35:51 +0900

    3.5.28

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,5 @@
-<script setup lang="ts">
-definePageMeta({
-    title: 'About use-bootstrap',
-    description: 'Quickly get a project started with any of our examples ranging from using parts of the framework to custom components and layouts.'
-})
-</script>
-
 <template>
-    <h2>First step</h2>
-    <b-p font-size="5">
-        usebootstrap is
-        <Typed class="text-primary fw-semibold" :strings="['powerful.', 'extensible.', 'feature-packed.']"
-            :type-speed="60" :loop="true" :back-speed="25" :back-delay="1500" />
-    </b-p>
-    <h2>Second step</h2>
-
+<b-p font-size="5">
+    Static fragment HTML string rendering.
+</b-p>
 </template>
